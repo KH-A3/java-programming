64be2935a6b8630dd4fb5d131d24d3748e778f2e
안병현 <abhyuni@gmail.com>
2022-03-22 19:46:06 +0900

    클래스 다이어 그램 문제/풀이

---
+++ j_pro05/src/exam10/model/vo/Member.java
@@ -0,0 +1,22 @@
+package exam10.model.vo;
+
+public class Member {
+
+	private String memberId;
+	private String memberPwd;
+	private String memberName;
+	private int age;
+	private char gender;
+	private String phone;
+	private String email;
+	
+	// 기본 생성자는 매개변수가 있는 생성자가 없으면 자동 생성
+	
+	public void changeName(String name) {
+		memberName = name;
+	}
+	
+	public void printName() {
+		System.out.println(memberName);
+	}
+}
