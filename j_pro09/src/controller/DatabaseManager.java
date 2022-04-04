bcd4395a6a106ccc6c34aed2436f149fb6d0ecba
안병현 <abhyuni@gmail.com>
2022-04-04 20:18:37 +0900

    조회 기능 구현

--- j_pro09/src/controller/DatabaseManager.java
+++ j_pro09/src/controller/DatabaseManager.java
@@ -5,9 +5,35 @@ import model.vo.Student;
 
 public class DatabaseManager implements ImplDatabaseManager {
 
+	private Student[] datas;
+	
+	// 초기화 블록
+	{
+		datas = new Student[5];
+		datas[0] = new Student("홍길동");	datas[1] = new Student("김도원");
+		datas[2] = new Student("박수아");	datas[3] = new Student("최종희");
+		datas[4] = new Student("이정우");
+		
+		for(int i = 0; i < datas.length; i++) {
+			datas[i].setGrades(new Grade[] {
+					new Grade("국어"), new Grade("영어"), new Grade("수학"), new Grade("과학")
+			});
+		}
+	}
+			
 	@Override
 	public Grade[] search(String name) {
-		return null;
+		int idx = -1;
+		for(int i = 0; i < datas.length; i++) {
+			if(name.equals(datas[i].getName())) {
+				idx = i;
+			}
+		}
+		if(idx == -1) {
+			return null;
+		} else {
+			return datas[idx].getGrades();
+		}
 	}
 
 	@Override
--- j_pro09/src/controller/MenuManager.java
+++ j_pro09/src/controller/MenuManager.java
@@ -1,15 +1,40 @@
 package controller;
 
+import java.util.Scanner;
+
+import model.vo.Grade;
+
 public class MenuManager {
+	
+	private Scanner sc = new Scanner(System.in);
+	private DatabaseManager db = new DatabaseManager();
+	
 	public void main() {
-		String menu = "";
-		menu += "1. 성적 조회\n";
-		menu += "2. 학생 정보 추가\n";
-		menu += "3. 성적 정보 수정\n";
-		menu += "4. 학생 정보 삭제\n";
-		menu += "9. 프로그램 종료\n";
-		menu += ">>>";
-		System.out.print(menu);
+		StringBuilder sb = new StringBuilder();
+		sb.append("1. 성적 조회\n");
+		sb.append("2. 학생 정보 추가\n");
+		sb.append("3. 성적 정보 수정\n");
+		sb.append("4. 학생 정보 삭제\n");
+		sb.append("9. 프로그램 종료\n");
+		sb.append(">>> ");
+		
+		while(true) {
+			System.out.print(sb.toString());
+			int menuNumber = Integer.parseInt(sc.nextLine());
+			
+			switch(menuNumber) {
+				case 1:
+					searchMenu();	break;
+				case 2:
+					studentAddMenu();	break;
+				case 3:
+					subjectModifyMenu();	break;
+				case 4:
+					studentDeleteMenu();	break;
+				case 9:
+					System.exit(0);
+			}
+		}
 		
 		/*
 		 * 사용자 입력을 받아서 위의 메뉴에 해당하는 번호를 입력하면 다음의 메서드를 동작
@@ -21,6 +46,18 @@ public class MenuManager {
 		 */
 	}
 	
+	public void studentDeleteMenu() {
+		System.out.println("학생 정보 삭제 메뉴 동작!");
+	}
+
+	public void subjectModifyMenu() {
+		System.out.println("성적 정보 수정 메뉴 동작!");
+	}
+
+	public void studentAddMenu() {
+		System.out.println("학생 정보 추가 메뉴 동작!");
+	}
+
 	public void searchMenu() {
 		/*
 		 * DatabaseManager 클래스의 search 메서드를 사용하여 성적 정보가 출력될 수 있게 한다.
@@ -35,5 +72,38 @@ public class MenuManager {
 		 * 평균 : xx.xx 점
 		 * --------------------
 		 */
+		while(true) {
+			System.out.print("학생 이름 : ");
+			String name = sc.nextLine();
+			Grade[] grades = db.search(name);
+			if(grades == null) {
+				System.out.println("해당 학생 정보가 존재하지 않습니다. 다시 입력하세요.");
+			} else {
+				StringBuilder sb = new StringBuilder();
+				sb.append("이름 : " + name + "\n");
+				sb.append("--------------------\n");
+				for(int i = 0; i < grades.length; i++) {
+					sb.append(grades[i].getName() + "\t");
+				}
+				sb.append("\n");
+				double avg = 0;
+				for(int i = 0; i < grades.length; i++) {
+					sb.append(grades[i].getScore() + "점\t");
+					avg += grades[i].getScore();
+				}
+				avg /= grades.length;
+				sb.append("\n");
+				for(int i = 0; i < grades.length; i++) {
+					sb.append(grades[i].getLevel() + "등급\t");
+				}
+				sb.append("\n");
+				sb.append("--------------------\n");
+				sb.append("평균 : " + avg + "\n");
+				sb.append("--------------------\n");
+				
+				System.out.println(sb.toString());
+				break;
+			}
+		}
 	}
 }
