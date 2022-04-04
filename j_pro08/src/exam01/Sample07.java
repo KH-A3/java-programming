20e36df6e0f387b867487f4756bbeca5144d4624
안병현 <abhyuni@gmail.com>
2022-04-04 16:59:34 +0900

    풀이

--- j_pro08/src/exam01/Sample07.java
+++ j_pro08/src/exam01/Sample07.java
@@ -2,6 +2,7 @@ package exam01;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -119,32 +120,107 @@ public class Sample07 {
 		System.out.println(now.after(birthDay));
 		System.out.println(now.before(birthDay));
 	}
+	
+	public void ex05() {
+		/*
+		 * 프로그램이 동작한 후 부터 종료할 때까지의 시간 기록을 남기기 위한 코드를 작성하시오.
+		 * 기록을 남기기 위한 프로그램은 1 ~ 100,000 까지의 누적합을 구하는 코드로 한다.
+		 */
+		Date start = new Date();
+		
+		long tot = 0;
+		for(long i = 1; i <= 10000000000L; i++) {
+			tot += i;
+		}
+		
+		Date end = new Date();
+		
+		long time = end.getTime() - start.getTime();
+		System.out.println("실행 시간 : " + time / 1000.0 + " 초");
+		
+	}
+	
+	public void ex06() {
+		/*
+		 * 2020년 부터 2099년 까지의 년도 중 윤년에 해당하는 년도를 정수 배열에 담아 출력하시오.
+		 */
+		GregorianCalendar gc = new GregorianCalendar();
+		int[] leapYear = new int[0];
+		for(int year = 2020; year < 2100; year++) {
+			if(gc.isLeapYear(year)) {
+				leapYear = Arrays.copyOf(leapYear, leapYear.length + 1);
+				leapYear[leapYear.length - 1] = year;
+			}
+		}
+		System.out.println(Arrays.toString(leapYear));
+	}
+	
+	public void ex07() {
+		/*
+		 * 2022년 04월 에서 주말에 해당하는 날짜만 날짜 배열에 저장하시오.
+		 */
+		Date[] dArr = new Date[0];
+		GregorianCalendar gc = new GregorianCalendar();
+		SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd E요일");
+		gc.set(2022, 3, 1);
+		
+		for(int i = gc.get(Calendar.DATE); i <= gc.getActualMaximum(Calendar.DATE); i++) {
+			gc.set(2022, 3, i);
+			switch(gc.get(Calendar.DAY_OF_WEEK)) {
+				case Calendar.SATURDAY:	case Calendar.SUNDAY:
+					dArr = Arrays.copyOf(dArr, dArr.length + 1);
+					dArr[dArr.length - 1] = gc.getTime();
+			}
+		}
+		
+		for(int i = 0; i < dArr.length; i++) {
+			System.out.println(sFormat.format(dArr[i]));
+		}
+	}
+	
+	public void ex08() {
+		/*
+		 * 유튜브에서는 영상 업로드가 언제 이루어졌는지를 보여주는 형식이 x초 전 또는 x분 전, x시간 전
+		 * x일 전, x주 전, x개월 전, y년 전 으로만 출력이 이루어지는데. 이를 동일하게 모방하여 사용자가
+		 * 입력한 날짜가 현재 시간과 얼만큼의 차이가 있는지 출력하시오.
+		 */
+		SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd a hh:mm:ss");
+		GregorianCalendar upload = new GregorianCalendar(2021, 2 - 1, 28, 12, 46, 20);
+		Date now = new Date();
+		
+		System.out.println("업로드 시간 : " + sFormat.format(upload.getTime()));
+		System.out.println("현재 시간 : " + sFormat.format(now));
+		
+		int beforeTime = (int)((now.getTime() - upload.getTime().getTime()) / 1000);
+		
+		if(beforeTime <= 60) {
+			System.out.println(beforeTime + " 초 전");
+		} else if(beforeTime <= 60 * 60) {
+			System.out.println(beforeTime / 60 + " 분 전");
+		} else if(beforeTime <= 60 * 60 * 24) {
+			System.out.println(beforeTime / (60 * 60) + " 시간 전");
+		} else if(beforeTime <= 60 * 60 * 24 * 7) {
+			System.out.println(beforeTime / (60 * 60 * 24) + " 일 전");
+		} else if(beforeTime <= 60 * 60 * 24 * 7 * 4) {
+			System.out.println(beforeTime / (60 * 60 * 24 * 7) + " 주 전");
+		} else if(beforeTime <= 60 * 60 * 24 * 7 * 4 * 12) {
+			System.out.println(beforeTime / (60 * 60 * 24 * 7 * 4) + " 개월 전");
+		} else {
+			System.out.println(beforeTime / (60 * 60 * 24 * 7 * 4 * 12) + " 년 전");
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		Sample07 sample = new Sample07();
 		// sample.ex01();
 		// sample.ex02();
 		// sample.ex03();
-		sample.ex04();
-		
-		/*
-		 * 프로그램이 동작한 후 부터 종료할 때까지의 시간 기록을 남기기 위한 코드를 작성하시오.
-		 * 기록을 남기기 위한 프로그램은 1 ~ 100,000 까지의 누적합을 구하는 코드로 한다.
-		 */
-		
-		/*
-		 * 2020년 부터 2099년 까지의 년도 중 윤년에 해당하는 년도를 정수 배열에 담아 출력하시오.
-		 */
-		
-		/*
-		 * 2022년 04월 에서 주말에 해당하는 날짜만 날짜 배열에 저장하시오.
-		 */
-		
-		/*
-		 * 유튜브에서는 영상 업로드가 언제 이루어졌는지를 보여주는 형식이 x초 전 또는 x분 전, x시간 전
-		 * x일 전, x주 전, x개월 전, y년 전 으로만 출력이 이루어지는데. 이를 동일하게 모방하여 사용자가
-		 * 입력한 날짜가 현재 시간과 얼만큼의 차이가 있는지 출력하시오.
-		 */
+		// sample.ex04();
+		// sample.ex05();
+		// sample.ex06();
+		// sample.ex07();
+		sample.ex08();
 		
 		/*
 		 * D-Day 계산기를 만들어 보시오.
