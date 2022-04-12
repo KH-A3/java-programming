714faa83ea210cc34fa7563f9e14eb037c95d0bd
안병현 <abhyuni@gmail.com>
2022-04-12 16:44:02 +0900

    파일 클래스

--- j_pro13/src/exam01/Sample01.java
+++ j_pro13/src/exam01/Sample01.java
@@ -1,5 +1,11 @@
 package exam01;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
 public class Sample01 {
 
 	public static void main(String[] args) {
@@ -20,6 +26,63 @@ public class Sample01 {
 		 *     - 바이트기반스트림의 경우 일반 텍스트 문서를 제외한 모든 파일을 입/출력하기 위해 사용
 		 *       (영상, 음향, 이미지, 각종 실행파일 등...)
 		 */
+		
+		/*
+		 * File 클래스
+		 *     - 파일의 크기, 이름, 정보 등을 알아내기 위한 클래스로 사용
+		 *     - 파일 생성, 삭제 및 폴더 생성 삭제 기능을 제공
+		 */
+		File f = new File("C:/Users/user2/eclipse/jee-2021-12/eclipse/eclipse.ini");
+		
+		System.out.printf("파일 크기 : %d Byte\n", f.length());
+		System.out.printf("파일 크기 : %.2f KByte\n", f.length() / 1024.0);
+		
+		SimpleDateFormat sFormat = new SimpleDateFormat("yyyy년 MM월 dd일 a hh시 mm분 ss초");
+		String modifyDate = sFormat.format(new Date(f.lastModified()));
+		System.out.printf("수정 날짜 : %s\n", modifyDate);
+		
+		System.out.printf("실행 파일 : %s\n", f.canExecute());
+		System.out.printf("읽기 가능 : %s\n", f.canRead());
+		System.out.printf("쓰기 가능 : %s\n", f.canWrite());
+		
+		System.out.printf("파일 : %s\n", f.isFile());
+		System.out.printf("폴더 : %s\n", f.isDirectory());
+		System.out.printf("숨김 : %s\n", f.isHidden());
+		
+		System.out.printf("파일/폴더명 : %s\n", f.getName());
+		System.out.printf("상위 폴더명 : %s\n", f.getParent());
+		System.out.printf("전체 경로명 : %s\n", f.getPath());
+		
+		System.out.printf("실제로 존재하는 파일/폴더 유무 : %s\n", f.exists());
+		
+		File f2 = new File("C:/Users/user2/eclipse/jee-2021-12/eclipse/my_file.wav");
+		try {
+			f2.createNewFile();
+		} catch (IOException e) {
+			System.out.println("my_file.txt 파일 생성문 에러 발생!");
+			e.printStackTrace();
+		}
+		
+		File f3 = new File("C:/Users/user2/eclipse/jee-2021-12/eclipse/my_folder");
+		f3.mkdir();
+		
+		File f4 = new File("C:/Users/user2/eclipse/jee-2021-12/eclipse/my_file.txt");
+		f4.delete();
+		
+		File f5 = new File("C:/Users/user2/eclipse/jee-2021-12/eclipse");
+		String[] strFiles = f5.list();
+		File[] fFiles = f5.listFiles();
+		
+		System.out.println(Arrays.toString(strFiles));
+		System.out.println(Arrays.toString(fFiles));
+		
+		for(int i = 0; i < fFiles.length; i++) {
+			String name = fFiles[i].getName();
+			if(name.startsWith("my_")) {
+				System.out.println(fFiles[i].getPath() + " 파일을 삭제하였습니다.");
+				fFiles[i].delete();
+			}
+		}
 	}
 
 }
