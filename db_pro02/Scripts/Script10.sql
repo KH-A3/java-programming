e2a48adec95073355c7d5e0d8a8958a72f24d012
안병현 <abhyuni@gmail.com>
2022-05-04 17:38:06 +0900

    SYNONMY

---
+++ db_pro02/Scripts/Script10.sql
@@ -0,0 +1,19 @@
+-- 사전 작업으로 PUSER1, DEVADMIN 계정에 SYNONYM 를 생성할 권한을 부여.;
+GRANT CREATE SYNONYM TO PUSER1;
+GRANT CREATE SYNONYM TO DEVADMIN;
+
+-- 비공개 동의어(PUSER1 에서 만듬.)
+CREATE SYNONYM EMP1 FOR EMPLOYEES;
+
+-- 공개 동의어(관리자 계정에서만 사용 가능)
+CREATE PUBLIC SYNONYM EMP2 FOR PUSER1.EMPLOYEES;
+
+-- 비공개/공개 동의어를 다른 계정에서 사용하기 위해 최소한 SELECT 권한은 필요하다.
+GRANT SELECT ON EMPLOYEES TO DEVADMIN;
+
+-- PUSER1이 생성한 비공개 동의어를 DEVADMIN 이 접근하여 사용.
+SELECT * FROM PUSER1.EMP1;
+
+-- 관리자가 생성한 공개 동의어를 DEVADMIN 이 접근하여 사용
+SELECT * FROM EMP2;
+
