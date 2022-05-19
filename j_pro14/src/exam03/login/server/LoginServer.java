f4416e8706edd13f41c7e9624efab318f7811df0
안병현 <abhyuni@gmail.com>
2022-05-19 21:42:03 +0900

    웹 프로젝트 추가

---
+++ j_pro14/src/exam03/login/server/LoginServer.java
@@ -0,0 +1,16 @@
+package exam03.login.server;
+
+import java.util.StringTokenizer;
+
+public class LoginServer {
+
+	/*
+	 * 로그인 서버
+	 *     1. 클라이언트가 아이디와 패스워드로 연결 요청을 하면
+	 *        account.db 파일에 있는 사용자 정보를 확인
+	 *     2. account.db 파일에 동일한 아이디, 패스워드가 존재하는
+	 *        경우 로그인 승낙을 함.
+	 *     3. account.db 파일에 동일한 아이디, 패스워드가 없는 경우
+	 *        연결 끊음.
+	 */
+}
