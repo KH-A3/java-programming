a384cf761f86bbf0a59182274e072040f9c924be
안병현 <abhyuni@gmail.com>
2022-07-11 21:41:40 +0900

    JSP/Servlet Emp 추가

---
+++ jsp_pro01/src/main/java/emps/model/EmpsDAO.java
@@ -0,0 +1,48 @@
+package emps.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.conn.db.DBConn;
+
+public class EmpsDAO {
+	private SqlSession session = DBConn.getSqlSession();
+	private String mapper = "empsMapper.%s";
+	
+	public List<EmpsDTO> selectAll() {
+		String mapId = String.format(mapper, "selectAll");
+		List<EmpsDTO> datas = session.selectList(mapId);
+		return datas;
+	}
+
+	public List<EmpsDTO> selectPage(int start, int end) {
+		String mapId = String.format(mapper, "selectPage");
+		Map<String, Integer> page = new HashMap<String, Integer>();
+		page.put("start", start);
+		page.put("end", end);
+		List<EmpsDTO> datas = session.selectList(mapId, page);
+		return datas;
+	}
+	
+	public int rowCount() {
+		String mapId = String.format(mapper, "rowCount");
+		int count = session.selectOne(mapId);
+		return count;
+	}
+	
+	public void commit() {
+		session.commit();
+	}
+	
+	public void rollback() {
+		session.rollback();
+	}
+	
+	public void close() {
+		session.close();
+	}
+
+}
