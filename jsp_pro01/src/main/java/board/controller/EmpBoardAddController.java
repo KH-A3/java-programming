1bdfdb48dd2281c3e213864d69a89717eb798d66
안병현 <abhyuni@gmail.com>
2022-07-28 15:50:37 +0900

    close #9 close #11 close #15 close #21
    로그인 필터에 등록 및 테스트 까지 완료 함.

--- jsp_pro01/src/main/java/board/controller/EmpBoardAddController.java
+++ jsp_pro01/src/main/java/board/controller/EmpBoardAddController.java
@@ -34,8 +34,7 @@ public class EmpBoardAddController extends HttpServlet {
 		EmpBoardDTO data = new EmpBoardDTO();
 		data.setTitle(title);
 		data.setContent(content);
-		// data.setEmpId(((EmpsDTO)session.getAttribute("loginData")).getEmpId());
-		data.setEmpId(100);
+		data.setEmpId(((EmpsDTO)session.getAttribute("loginData")).getEmpId());
 		
 		int boardId = service.add(data);
 		
